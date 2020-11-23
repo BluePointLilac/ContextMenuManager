b8d9023efc9c755b2481854fafdeedb3e6ab47de
蓝点lilac <yourena0528@gmail.com>
2020-11-24 05:21:48 +0800

    将添加常用菜单模块移动到其他规则

---
+++ ContextMenuManager/Controls/EnhanceMenusItem.cs
@@ -0,0 +1,109 @@
+﻿using BulePointLilac.Controls;
+using BulePointLilac.Methods;
+using ContextMenuManager.Controls.Interfaces;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace ContextMenuManager.Controls
+{
+    sealed class EnhanceShellItem : MyListItem, IFoldSubItem, IChkVisibleItem
+    {
+        public string RegPath { get; set; }
+        public XmlElement ItemXE { get; set; }
+        public VisibleCheckBox ChkVisible { get; set; }
+        public IFoldGroupItem FoldGroupItem { get; set; }
+        public bool ItemVisible
+        {
+            get
+            {
+                using(RegistryKey key = RegistryEx.GetRegistryKey(RegPath))
+                    return key != null;
+            }
+            set
+            {
+                if(value) WriteSubKeysValue(ItemXE, RegPath);
+                else RegistryEx.DeleteKeyTree(RegPath);
+            }
+        }
+
+        public EnhanceShellItem()
+        {
+            ChkVisible = new VisibleCheckBox(this);
+        }
+
+        private static void WriteAttributesValue(XmlNode valueXN, string regPath)
+        {
+            if(valueXN == null) return;
+            XmlNode szXN = valueXN.SelectSingleNode("REG_SZ");
+            XmlNode dwordXN = valueXN.SelectSingleNode("REG_DWORD");
+            XmlNode expand_szXN = valueXN.SelectSingleNode("REG_EXPAND_SZ");
+            if(szXN != null)
+                foreach(XmlAttribute a in szXN.Attributes)
+                    Registry.SetValue(regPath, a.Name, a.Value, RegistryValueKind.String);
+            if(expand_szXN != null)
+                foreach(XmlAttribute a in expand_szXN.Attributes)
+                    Registry.SetValue(regPath, a.Name, a.Value, RegistryValueKind.ExpandString);
+            if(dwordXN != null)
+                foreach(XmlAttribute a in dwordXN.Attributes)
+                {
+                    int value = a.Value.StartsWith("0x", StringComparison.OrdinalIgnoreCase)
+                        ? Convert.ToInt32(a.Value, 16) : Convert.ToInt32(a.Value);
+                    Registry.SetValue(regPath, a.Name, value, RegistryValueKind.DWord);
+                }
+        }
+
+        private static void WriteSubKeysValue(XmlElement keyXE, string regPath)
+        {
+            if(keyXE == null) return;
+            string defaultValue = keyXE.GetAttribute("Default");
+            if(!defaultValue.IsNullOrWhiteSpace()) Registry.SetValue(regPath, "", defaultValue);
+            WriteAttributesValue(keyXE.SelectSingleNode("Value"), regPath);
+
+            XmlNode subKeyXN = keyXE.SelectSingleNode("SubKey");
+            if(subKeyXN != null)
+            {
+                foreach(XmlElement xe in subKeyXN.ChildNodes)
+                    WriteSubKeysValue(xe, $@"{regPath}\{xe.Name}");
+            }
+        }
+    }
+
+    sealed class EnhanceShellExItem : MyListItem, IFoldSubItem, IChkVisibleItem
+    {
+        public string ShellExPath { get; set; }
+        public string DefaultKeyName { get; set; }
+        public Guid Guid { get; set; }
+        public VisibleCheckBox ChkVisible { get; set; }
+        public IFoldGroupItem FoldGroupItem { get; set; }
+        public bool ItemVisible
+        {
+            get => ShellExItem.GetPathAndGuids(ShellExPath).Values.Contains(Guid);
+            set
+            {
+                if(value)
+                {
+                    string regPath = ObjectPath.GetNewPathWithIndex
+                    ($@"{ShellExPath}\ContextMenuHandlers\{DefaultKeyName}", ObjectPath.PathType.Registry);
+                    Registry.SetValue(regPath, "", this.Guid.ToString("B"));
+                }
+                else
+                {
+                    Dictionary<string, Guid> dic = ShellExItem.GetPathAndGuids(ShellExPath);
+                    foreach(string regPath in dic.Keys)
+                    {
+                        if(dic[regPath].Equals(this.Guid))
+                            RegistryEx.DeleteKeyTree(regPath);
+                    }
+                }
+            }
+        }
+
+        public EnhanceShellExItem()
+        {
+            ChkVisible = new VisibleCheckBox(this);
+        }
+    }
+}
---
+++ ContextMenuManager/Controls/EnhanceMenusList.cs
@@ -0,0 +1,197 @@
+﻿using BulePointLilac.Controls;
+using BulePointLilac.Methods;
+using ContextMenuManager.Controls.Interfaces;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace ContextMenuManager.Controls
+{
+    sealed class EnhanceMenusList : MyList
+    {
+        public void LoadItems()
+        {
+            this.ClearItems();
+            XmlDocument doc = ReadXml();
+            foreach(XmlNode xn in doc.DocumentElement.ChildNodes)
+            {
+                string path = null;
+                string text = null;
+                Image image = null;
+                switch(xn.Name)
+                {
+                    case "File":
+                        path = ShellList.MENUPATH_FILE;
+                        text = AppString.SideBar.File;
+                        image = AppImage.File;
+                        break;
+                    case "Folder":
+                        path = ShellList.MENUPATH_FOLDER;
+                        text = AppString.SideBar.Folder;
+                        image = AppImage.Folder;
+                        break;
+                    case "Directory":
+                        path = ShellList.MENUPATH_FOLDER;
+                        text = AppString.SideBar.Directory;
+                        image = AppImage.Directory;
+                        break;
+                    case "Background":
+                        path = ShellList.MENUPATH_BACKGROUND;
+                        text = AppString.SideBar.Background;
+                        image = AppImage.Background;
+                        break;
+                    case "Desktop":
+                        path = ShellList.MENUPATH_DESKTOP;
+                        //Vista没有桌面右键菜单的独立注册表项
+                        if(WindowsOsVersion.IsEqualVista) path = ShellList.MENUPATH_BACKGROUND;
+                        text = AppString.SideBar.Desktop;
+                        image = AppImage.Desktop;
+                        break;
+                    case "Drive":
+                        path = ShellList.MENUPATH_DRIVE;
+                        text = AppString.SideBar.Drive;
+                        image = AppImage.Drive;
+                        break;
+                    case "AllObjects":
+                        path = ShellList.MENUPATH_ALLOBJECTS;
+                        text = AppString.SideBar.AllObjects;
+                        image = AppImage.AllObjects;
+                        break;
+                    case "Computer":
+                        path = ShellList.MENUPATH_COMPUTER;
+                        text = AppString.SideBar.Computer;
+                        image = AppImage.Computer;
+                        break;
+                    case "RecycleBin":
+                        path = ShellList.MENUPATH_RECYCLEBIN;
+                        text = AppString.SideBar.RecycleBin;
+                        image = AppImage.RecycleBin;
+                        break;
+                    default:
+                        XmlElement xe = (XmlElement)xn;
+                        path = xe.GetAttribute("RegPath");
+                        text = ResourceString.GetDirectString(xe.GetAttribute("Text"));
+                        image = ResourceIcon.GetIcon(xe.GetAttribute("Icon"))?.ToBitmap() ?? AppImage.NotFound;
+                        break;
+                }
+                if(string.IsNullOrEmpty(path) || string.IsNullOrEmpty(text)) continue;
+                GroupPathItem groupItem = new GroupPathItem
+                {
+                    PathType = ObjectPath.PathType.Registry,
+                    TargetPath = path,
+                    Image = image,
+                    Text = text,
+                };
+                this.AddItem(groupItem);
+                XmlElement shellXE = (XmlElement)xn.SelectSingleNode("Shell");
+                XmlElement shellExXE = (XmlElement)xn.SelectSingleNode("ShellEx");
+                if(shellXE != null) LoadShellItems(shellXE, groupItem);
+                if(shellExXE != null) LoadShellExItems(shellExXE, groupItem);
+                groupItem.IsFold = true;
+            }
+
+        }
+
+        private XmlDocument ReadXml()
+        {
+            XmlDocument doc1 = new XmlDocument();
+            //如果没有网络下载的，则将程序内置的写入
+            if(!File.Exists(Program.AppDataEnhanceMenusDicPath))
+            {
+                File.WriteAllText(Program.AppDataEnhanceMenusDicPath, Properties.Resources.EnhanceMenusDic, Encoding.UTF8);
+            }
+            doc1.Load(Program.AppDataEnhanceMenusDicPath);
+            if(File.Exists(Program.EnhanceMenusDicPath))
+            {
+                XmlDocument doc2 = new XmlDocument();
+                doc2.Load(Program.EnhanceMenusDicPath);
+                foreach(XmlNode xn in doc2.DocumentElement.ChildNodes)
+                {
+                    XmlNode node = doc1.ImportNode(xn, true);
+                    doc1.DocumentElement.AppendChild(node);
+                }
+            }
+            return doc1;
+        }
+
+        private void LoadShellItems(XmlElement shellXE, GroupPathItem groupItem)
+        {
+            foreach(XmlElement itemXE in shellXE.GetElementsByTagName("Item"))
+            {
+                XmlElement verXE = (XmlElement)itemXE.SelectSingleNode("OSVersion");
+                if(!JudgeOSVersion(verXE)) continue;
+                XmlElement szXE = (XmlElement)itemXE.SelectSingleNode("Value/REG_SZ");
+                string keyName = itemXE.GetAttribute("KeyName");
+                if(keyName.IsNullOrWhiteSpace()) continue;
+                string regPath = $@"{groupItem.TargetPath}\shell\{keyName}";
+                EnhanceShellItem item = new EnhanceShellItem()
+                {
+                    RegPath = $@"{groupItem.TargetPath}\shell\{keyName}",
+                    FoldGroupItem = groupItem,
+                    ItemXE = itemXE
+                };
+                if(szXE != null)
+                {
+                    item.Text = ResourceString.GetDirectString(szXE.GetAttribute("MUIVerb"));
+                    if(szXE.HasAttribute("Icon")) item.Image = ResourceIcon.GetIcon(szXE.GetAttribute("Icon"))?.ToBitmap();
+                    else if(szXE.HasAttribute("HasLUAShield")) item.Image = AppImage.Shield;
+                }
+                if(item.Image == null) item.Image = AppImage.NotFound;
+                if(item.Text.IsNullOrWhiteSpace()) item.Text = keyName;
+                item.ChkVisible.Checked = item.ItemVisible;
+                MyToolTip.SetToolTip(item.ChkVisible, itemXE.GetAttribute("Tip"));
+                this.AddItem(item);
+            }
+        }
+
+        private void LoadShellExItems(XmlElement shellExXE, GroupPathItem groupItem)
+        {
+            foreach(XmlElement itemXE in shellExXE.GetElementsByTagName("Item"))
+            {
+                XmlElement verXE = (XmlElement)itemXE.SelectSingleNode("OSVersion");
+                if(!JudgeOSVersion(verXE)) continue;
+                if(!GuidInfo.TryGetGuid(itemXE.GetAttribute("Guid"), out Guid guid)) continue;
+                EnhanceShellExItem item = new EnhanceShellExItem
+                {
+                    FoldGroupItem = groupItem,
+                    ShellExPath = $@"{groupItem.TargetPath}\ShellEx",
+                    Image = ResourceIcon.GetIcon(itemXE.GetAttribute("Icon"))?.ToBitmap() ?? AppImage.DllDefaultIcon,
+                    Text = ResourceString.GetDirectString(itemXE.GetAttribute("Text")),
+                    DefaultKeyName = itemXE.GetAttribute("KeyName"),
+                    Guid = guid
+                };
+                if(item.Text.IsNullOrWhiteSpace()) item.Text = GuidInfo.GetText(guid);
+                if(item.DefaultKeyName.IsNullOrWhiteSpace()) item.DefaultKeyName = guid.ToString("B");
+                item.ChkVisible.Checked = item.ItemVisible;
+                MyToolTip.SetToolTip(item.ChkVisible, itemXE.GetAttribute("Tip"));
+                this.AddItem(item);
+            }
+        }
+
+        public static bool JudgeOSVersion(XmlElement osXE)
+        {
+            if(osXE == null) return true;
+            Version ver = new Version(osXE.InnerText);
+            Version osVer = Environment.OSVersion.Version;
+            int compare = osVer.CompareTo(ver);
+            string symbol = osXE.GetAttribute("Compare");
+            switch(symbol)
+            {
+                case ">":
+                    return compare > 0;
+                case "<":
+                    return compare < 0;
+                case "=":
+                    return compare == 0;
+                case ">=":
+                    return compare >= 0;
+                case "<=":
+                    return compare <= 0;
+                default:
+                    return true;
+            }
+        }
+    }
+}
